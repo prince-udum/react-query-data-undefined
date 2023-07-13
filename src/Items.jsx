473516abc7363042c9b5308c18f258a502173062
prince <princeudum1@gmail.com>
2023-07-13 17:58:21 +0100

    initial commit

---
+++ src/Items.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+
+const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  });
+  console.log(result);
+  return (
+    <div className="items">
+      {items.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items;
